1170b6886bc811c339b13685e4f940b1ab182c29
Shifuuu31 <asaadane@student.1337.ma>
2024-11-28 01:23:07 +0100

    add f finding paths, get valid combs logic

--- lemin/Find-Paths.go
+++ lemin/Find-Paths.go
@@ -1,18 +1,69 @@
 package lemin
 
 import (
+	"sort"
+
 	"Lemin-Project/variables"
 )
 
-func FindAllPaths() {
-	var path []variables.Room
-	path = append(path, *variables.Rooms[variables.Start])
+var Paths [][]string
 
-	if variables.Start == variables.End {
-		variables.Paths = append(variables.Paths, path)
+func getRoomName(currentRoom *variables.Room) string {
+	for rName, room := range variables.Rooms {
+		if room == currentRoom {
+			return rName
+		}
+	}
+	return ""
+}
+
+func ExtractPaths(currentRoom *variables.Room, Qpath []string) {
+	defer func() {
+		currentRoom.Visited = false
+	}()
+	currentRoom.Visited = true
+	Qpath = append(Qpath, getRoomName(currentRoom))
+	if currentRoom == variables.Rooms[variables.End] {
+		Paths = append(Paths, Qpath)
 	} else {
-		for _, v := range variables.Paths[len(variables.Paths)-1] {
-			if (!path.co)
+		for _, childRoom := range currentRoom.Relations {
+			if !childRoom.Visited {
+				if childRoom == variables.Rooms[variables.End] {
+					Paths = append(Paths, append(Qpath, variables.End))
+					return
+				} else {
+					ExtractPaths(childRoom, Qpath)
+				}
+			}
 		}
 	}
 }
+
+func SortPaths() {
+	sort.Slice(Paths, func(i, j int) bool {
+		return len(Paths[i]) < len(Paths[j])
+	})
+}
+
+// -------------------------
+// using room pointer as indicator
+// -------------------------
+// var Paths [][]*variables.Room
+
+// func ExtractPaths(currentRoom *variables.Room, Qpath []*variables.Room) {
+// 	defer func() {
+// 		currentRoom.Visited = false
+// 	}()
+// 	currentRoom.Visited = true
+// 	Qpath = append(Qpath, currentRoom)
+// 	if currentRoom == variables.Rooms[variables.End] {
+// 		Paths = append(Paths, Qpath)
+// 	} else {
+// 		for _, childRoom := range currentRoom.Relations {
+// 			if !childRoom.Visited {
+// 				ExtractPaths(childRoom, Qpath)
+// 			}
+// 		}
+// 	}
+// }
+
--- main.go
+++ main.go
@@ -1,15 +1,86 @@
 package main
 
 import (
+	"fmt"
+
 	"Lemin-Project/lemin"
 	"Lemin-Project/variables"
-	"fmt"
 )
+
+func PrintInfos() {
+	for key, value := range variables.Rooms {
+		fmt.Printf("name: %v,  adrr: %p,  x: %v, y: %v, visited: %v\n\tRelations: [", key, value, value.X, value.Y, value.Visited)
+		for _, rel := range value.Relations {
+			for key, value := range variables.Rooms {
+				if value == rel {
+					fmt.Printf("%v - %p\t", key, value)
+				}
+			}
+		}
+		fmt.Println("]\n-------------------------------------------------------------------------------------------------------------------------------------------------")
+	}
+}
 
 func main() {
 	lemin.FindAll(&variables.Rooms)
-
-	for key, value := range variables.Rooms {
-		fmt.Printf("%v,  %p,  %v\n", key, value, value)
+	PrintInfos()
+	fmt.Println("\n-------------------------------------------------------------------------------------------------------------------------------------------------")
+	lemin.ExtractPaths(variables.Rooms[variables.Start], nil)
+	// lemin.ExtractPaths(variables.Rooms[variables.Start],[]*variables.Room{})
+	lemin.SortPaths()
+	for i, path := range lemin.Paths {
+		fmt.Printf("path n%d: %v >> capcity: %d\n", i+1, path, len(path)-2)
 	}
+	fmt.Println("\n-------------------------------------------------------------------------------------------------------------------------------------------------")
+	combined := ValidPathsCombs()
+	for i, com := range combined {
+		fmt.Printf("combination n%d: %v >> number of combined paths %d\n", i+1, com, len(com))
+	}
+	fmt.Println("\n-------------------------------------------------------------------------------------------------------------------------------------------------")
+	// for i, path := range lemin.Paths {
+	// 	fmt.Printf("path n%d: %v >> capcity: %d\n", i+1, path, len(path)-2)
+	// }
+}
+
+func ValidPathsCombs() (validCombs [][][]string) {
+	for i := range lemin.Paths {
+		var currComb [][]string
+		currComb = append(currComb, lemin.Paths[i])
+
+		for j, path := range lemin.Paths {
+			if i == j {
+				continue
+			}
+			isInter := false
+			for _, currCombPath := range currComb {
+				if pathsIntersect(currCombPath, path) {
+					isInter = true
+					break
+				}
+			}
+			if !isInter {
+				currComb = append(currComb, path)
+			}
+		}
+		validCombs = append(validCombs, currComb)
+	}
+	return validCombs
+}
+
+func pathsIntersect(path1, path2 []string) bool {
+	for _, roomName := range path1[1 : len(path1)-1] {
+		if containsRoom(path2, roomName) {
+			return true
+		}
+	}
+	return false
+}
+
+func containsRoom(path []string, roomName string) bool {
+	for _, room := range path[1 : len(path)-1] {
+		if room == roomName {
+			return true
+		}
+	}
+	return false
 }
